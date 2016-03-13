680302a8e25ed121bd5ab113043997692a446280
wangjulong <429638836@qq.com>
2016-03-13 09:43:59 +0800

    Lesson 13 配置时间日期组件

--- backend/modules/settings/views/companies/_form.php
+++ backend/modules/settings/views/companies/_form.php
@@ -1,5 +1,6 @@
 <?php
 
+use dosamigos\datepicker\DatePicker;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -18,7 +19,17 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'company_address')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'company_start_date')->textInput() ?>
+    <?= $form->field($model, 'company_start_date')->widget(
+        DatePicker::className(), [
+        // inline too, not bad
+        'inline' => false,
+        // modify template for custom rendering
+        //'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+        'clientOptions' => [
+            'autoclose' => true,
+            'format' => 'yyyy-mm-dd'
+        ]
+    ]);?>
 
     <?= $form->field($model, 'company_created_date')->textInput() ?>
 
