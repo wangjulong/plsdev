867d02821f0b1a5b2f81ce2306170a1e41f15d08
wangjulong <429638836@qq.com>
2016-03-13 11:15:26 +0800

    Yii2 Lesson - 14 设置 _form(create 和 update 可以使用) https://github.com/kartik-v/yii2-widget-select2 根据官网设置

--- backend/views/branches/_form.php
+++ backend/views/branches/_form.php
@@ -1,6 +1,7 @@
 <?php
 
 use backend\models\Companies;
+use kartik\select2\Select2;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
@@ -14,10 +15,16 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'companies_company_id')->dropDownList(
-        ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name'),
-        ['prompt' => 'Select Company']
-    ) ?>
+
+
+    <?= $form->field($model, 'companies_company_id')->widget(Select2::classname(), [
+        'data' => ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name'),
+        'language' => 'en',
+        'options' => ['placeholder' => 'Select a state ...'],
+        'pluginOptions' => [
+            'allowClear' => true
+        ],
+    ]) ?>
 
     <?= $form->field($model, 'branch_name')->textInput(['maxlength' => true]) ?>
 
